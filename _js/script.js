77123e7429da215c7cd8c8fa09814229eea6f7ea
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-07-24 22:10:29 -0300

    Criando function cadastrar com 'fech' e function limpar

--- _js/script.js
+++ _js/script.js
@@ -4,15 +4,35 @@ const iEmail = document.querySelector(".email");
 const iSenha = document.querySelector(".senha");
 const iTelefone = document.querySelector(".telefone");
 
+function cadastrar(){
+    fetch("http://localhost:8080/cadastrar",
+    {
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            nome: iNome.value,
+            email: iEmail.value,
+            senha: iSenha.value,
+            telefone: iTelefone.value
+           })
+    })
+    .then(function (res) { console.log(res) })
+    .catch(function (res) { console.log(res) })
+};
+
+function limpar(){
+    iNome.value = "";
+    iEmail.value = "";
+    iSenha.value = "";
+    iTelefone.value = "";
+}
+
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
 
-   const dados = {
-    nome: iNome.value,
-    email: iEmail.value,
-    senha: iSenha.value,
-    telefone: iTelefone.value
-   };
-   
-   console.log(dados);
+    cadastrar();
+    limpar();
 });
