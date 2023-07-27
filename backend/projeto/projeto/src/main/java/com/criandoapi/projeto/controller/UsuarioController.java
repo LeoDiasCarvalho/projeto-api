63772d539a2d0452f6ded3d49d0759ca643e0682
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-07-27 19:39:05 -0300

    Criando Model e DAO e o endpoint de buscar todos os usuário do banco

--- backend/projeto/projeto/src/main/java/com/criandoapi/projeto/controller/UsuarioController.java
+++ backend/projeto/projeto/src/main/java/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,14 +1,25 @@
 package com.criandoapi.projeto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.criandoapi.projeto.dao.IUsuario;
+import com.criandoapi.projeto.model.Usuario;
 
 @RestController
 public class UsuarioController {
 	
+	@Autowired
+	private IUsuario dao;
+	
+	//Método para retornar todos os usuário do banco de dados
 	@GetMapping("/usuarios")
-	public String texto() {
-		return "Acessando a API de Leo Dias";
+	public List<Usuario> findAll() {
+
+		return (List<Usuario>) dao.findAll();
 	}
 
 }
---
+++ backend/projeto/projeto/src/main/java/com/criandoapi/projeto/model/Usuario.java
@@ -0,0 +1,103 @@
+package com.criandoapi.projeto.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="id", length= 100, nullable = true)
+	private Integer id;
+	
+	@Column(name = "nome_completo", length = 100, nullable = true)
+	private String nome;
+	
+	@Column(name = "email", length = 100, nullable = true)
+	private String email;
+	
+	@Column(name = "senha", columnDefinition = "TEXT", nullable = true)
+	private String senha;
+	
+	@Column(name = "telefone", length = 15, nullable = true)
+	private String telefone;
+	
+	public Usuario() {
+	}
+
+	public Usuario(Integer id, String nome, String email, String senha, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.telefone = telefone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
