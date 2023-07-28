0f0ff09f5f1dca2b4d74a8f5721496be068c6e29
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-07-27 21:26:33 -0300

    Criando endpoint POST para salvar novos usuarios no banco

--- backend/projeto/projeto/src/main/java/com/criandoapi/projeto/controller/UsuarioController.java
+++ backend/projeto/projeto/src/main/java/com/criandoapi/projeto/controller/UsuarioController.java
@@ -4,22 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.criandoapi.projeto.dao.IUsuario;
 import com.criandoapi.projeto.model.Usuario;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 	
 	@Autowired
 	private IUsuario dao;
 	
 	//Método para retornar todos os usuário do banco de dados
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<Usuario> findAll() {
 
 		return (List<Usuario>) dao.findAll();
 	}
+	
+	//Método para salvar um usuário no banco
+	@PostMapping
+	public Usuario save(@RequestBody Usuario usuario) {
+		Usuario usuarioNovo = dao.save(usuario);
+		return usuarioNovo;
+	}
+	
+	
 
 }
